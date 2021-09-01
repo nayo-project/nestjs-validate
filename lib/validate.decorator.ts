6196515e09c183fafeaf65138f47cd4a0295ac1f
TerenceSm <527028803@qq.com>
2021-09-01 13:20:57 +0800

    更新nest包

--- lib/validate.decorator.ts
+++ lib/validate.decorator.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator } from '@nestjs/common';
 
 export const Validate = createParamDecorator((data, req) => {
-  return req.nestjsValidate;
+  return req["args"][0]["nestjsValidate"];
 });
